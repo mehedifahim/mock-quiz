9b0c4c06ca2745049e93fa3cbdfb6eada958ff0b
mehedihfahim <mehedii.fahim@gmail.com>
2022-10-24 03:13:20 +0600

    last commit

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Line, LineChart} from 'recharts';
-
+import { CartesianGrid, Line, LineChart, XAxis, YAxis} from 'recharts';
+import './Statistics.css'
 const Statistics = () => {
-    const data = useLoaderData();
-    const {total,name} =data;
+    const datas = useLoaderData();
+    const {data} =datas;
     console.log(data)
     return (
-        <div>
-            <LineChart width={400} height={400} data={data}>
+        
+        <div className='chart'>
+            <h1>Statistics:</h1>
+            <LineChart width={600} height={300} data={data}>
             <Line type="monotone" dataKey="total" stroke="#8884d8" />
-            </LineChart>
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis/>
+        </LineChart>
         </div>
     );
 };
