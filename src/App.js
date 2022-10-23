f8eb595473077888199e96505590dd3e79a6db8d
mehedihfahim <mehedii.fahim@gmail.com>
2022-10-24 02:44:09 +0600

    statistics

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
         },
         {
           path:'statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz') ,
+          
           element: <Statistics></Statistics>
         },
         {
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+    const {total,name} =data;
+    console.log(data)
     return (
         <div>
-            <h1>This is Statistics</h1>
+            <LineChart width={730} height={250} data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            
+            <XAxis dataKey={total} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="total" stroke="#8884d8" />
+            
+      </LineChart>
         </div>
     );
 };
