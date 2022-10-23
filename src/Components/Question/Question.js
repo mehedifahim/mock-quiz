760f6281e61770ee65b01ce5f6d73cfc1fe5b2c6
mehedihfahim <mehedii.fahim@gmail.com>
2022-10-24 01:59:06 +0600

    toast

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Option from '../Option/Option';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './Question.css'
 const Question = ({data,index}) => {
-    const {options, correctAnswer} = data
+    const {options, correctAnswer} = data;
+    const showAns = () =>{
+        
+        toast(correctAnswer)
+        
+    }
     return (
         <div>
             <div className='que-option'>
+            <button onClick={showAns}>
+            <FontAwesomeIcon icon={faEye}/>
+            <ToastContainer />
+            </button>
                 <div className='que'>
                     <h3>Quiz {index+1}: </h3>
                     <h3>{data.question}</h3>
